339658d2dff163553a2c0388b1b1fb2dad6b93b7
Krzysztof Glowacz <krzysztofglowacz88@gmail.com>
2024-04-16 18:05:37 +0200

    Added coments to the main.tf file, remove unused statements from the shell script

--- Cloud_app/infrastructure/images.sh
+++ Cloud_app/infrastructure/images.sh
@@ -1,28 +1,23 @@
-# Pull the Docker images
-    sudo docker pull krisenberg/backend:latest
-    sudo docker pull krisenberg/frontend:latest
-
 # Create the docker-compose file
-    cat <<DOCKER_EOF > docker-compose.yml
-    version: '3'
-    services:
-    backend:
-        image: krisenberg/backend:latest
-        ports:
-        - "8080:80"
-        environment:
-        - FRONTEND_IP=http://3.227.254.227:3000
-    frontend:
-        image: krisenberg/frontend:latest
-        ports:
-        - "3000:3000"
-        environment:
-        - REACT_APP_BACKEND_IP=http://3.227.254.227:8080
-    DOCKER_EOF
+cat <<DOCKER_EOF > docker-compose.yml
+version: '3'
+services:
+backend:
+    image: krisenberg/backend:latest
+    ports:
+    - "8080:80"
+    environment:
+    - FRONTEND_IP=http://3.227.254.227:3000
+frontend:
+    image: krisenberg/frontend:latest
+    ports:
+    - "3000:3000"
+    environment:
+    - REACT_APP_BACKEND_IP=http://3.227.254.227:8080
+DOCKER_EOF
 
-    sudo chmod +x docker-compose.yml
+# make the docker-compose file executable
+sudo chmod +x docker-compose.yml
 
-    # Start the Docker containers using docker-compose
-    sudo docker-compose up -d
-
-    echo "Docker containers started successfully."
+# run docker compose in a detached mode
+sudo docker-compose up -d
--- Cloud_app/infrastructure/main.tf
+++ Cloud_app/infrastructure/main.tf
@@ -9,123 +9,16 @@ terraform {
     required_version = ">= 1.2.0"
 }
 
-# Define the region in which AWS resources will 'physically' be created
+# Define the region in which AWS resources will 'physically' be created,
+# optionally add a profile name (from the file .aws/credentials)
 provider "aws" {
   region = "us-east-1"
   profile = "krzysztof.glowacz"
 }
 
-# Tworzy Virtual Private Cloud, które pozwala na DNS i nazwy hostów.
-# Włącza wsparcie dla DNS i nazw hostów DNS wewnątrz VPC
-resource "aws_vpc" "ttt_vpc" {
-  cidr_block           = "10.0.0.0/24"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-  tags = {
-    Name = "ttt_vpc"
-  }
-}
-
-# Brama umożliwiające łączenie się VPC z Internetem
-resource "aws_internet_gateway" "ttt_gateway" {
-  vpc_id = aws_vpc.ttt_vpc.id
-  tags = {
-    Name = "ttt_gateway"
-  }
-}
-
-# Tworzy podsieć w VPC z automatycznym przydzielaniem IP publicznych oraz dla EC2
-resource "aws_subnet" "ttt_subnet" {
-  vpc_id                  = aws_vpc.ttt_vpc.id
-  cidr_block              = "10.0.0.0/26"
-  map_public_ip_on_launch = true 
-  tags = {
-    Name = "ttt_subnet"
-  }
-}
-
-# Tworzy tabelę routingu dla VPC, dodając trasę domyślną przez bramę 
-resource "aws_route_table" "ttt_rt" { 
-  vpc_id = aws_vpc.ttt_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.ttt_gateway.id
-  }
-
-  tags = {
-    Name = "ttt_rt"
-  }
-}
-
-# Wiąże tabelę routingu z podsiecią, umożliwiając jej dostęp do internetu.
-resource "aws_route_table_association" "ttt_rt_assoc" {
-  subnet_id      = aws_subnet.ttt_subnet.id
-  route_table_id = aws_route_table.ttt_rt.id
-}
-
-# Tworzy grupę bezpieczeństwa z regułami dla ruchu przychodzącego (ingress) i wychodzącego (egress), umożliwiając jedynie ruch na niezbędnych portach.
-# Zapewnia to bezpieczeństwo przed atakami na wolne porty.
-resource "aws_security_group" "ttt_sec_group" {
-  name        = "ttt_sec_group"
-  vpc_id      = aws_vpc.ttt_vpc.id
-  description = "Security group for accessing application and ec2 via SSH"
-
-# HTTP
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-# HTTPS
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp" 
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-# backend
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp" 
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-# frontend (możnaby w sumie dawać front na port 80/443, ale zrobiłem inaczej)
-  ingress {
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp" 
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-# ssh
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Reguła dla całego ruchu wychodzącego, która go nie ogranicza
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "ttt_sec_group"
-  }
-}
-
-# Tworzy instancję EC2 z określonym obrazem AMI, typem instancji i grupą bezpieczeństwa.
-# AMI zostało znalezione dzięki stronie https://api.netcubed.de/latest/ami/lookup?platform=amzn2&fbclid=IwAR0BKcf3D28DiuvttUfKMZE0WtwgWRv0kW9_XCCJwHBntSRfG1LbcFyYK10_aem_AWHs8WBIRtX7NEjqT6j_cNuemLt6UmXLj0LJI1Zhp8B1CU_acHz7FYUNf_YAMdEoUpdU0QfAvXFSdC-_Ml61ic2E
+# Create the instance using some specific AMI, type t2.micro is perfectly fine for our needs
+# Attach the subnet, security group, (optionally ssh key name)
+# User data contains the script that will initially install docker and docker-compose on our instance
 resource "aws_instance" "tic_tac_toe" {
   ami                      = "ami-0cf43e890af9e3351"
   instance_type            = "t2.micro"
@@ -153,17 +46,111 @@ resource "aws_instance" "tic_tac_toe" {
     EOF
 }
 
-# Definicja i powiązanie Elastic IP, które daje nam publiczny adres IP
-# resource "aws_eip" "app_eip" {
-#   domain     = "vpc"
-#   depends_on = [aws_vpc.app_vpc]
-# }
+# Create Virtual Private Cloud and enable DNS and hostnames services inside it
+resource "aws_vpc" "ttt_vpc" {
+  cidr_block           = "10.0.0.0/24"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "ttt_vpc"
+  }
+}
 
-# resource "aws_eip_association" "eip_assoc" {
-#   instance_id   = aws_instance.tic_tac_toe.id
-#   allocation_id = aws_eip.app_eip.id
-# }
+# Attach an Internet gateway to make the instance visible from outside
+resource "aws_internet_gateway" "ttt_gateway" {
+  vpc_id = aws_vpc.ttt_vpc.id
+  tags = {
+    Name = "ttt_gateway"
+  }
+}
 
-# output "instance_public_ip" {
-#     value = aws_instance.tic_tac_toe.public_ip
-# }
+# Create a public subnet inside the instance
+resource "aws_subnet" "ttt_subnet" {
+  vpc_id                  = aws_vpc.ttt_vpc.id
+  cidr_block              = "10.0.0.0/26"
+  map_public_ip_on_launch = true 
+  tags = {
+    Name = "ttt_subnet"
+  }
+}
+
+# Create a routing table, attach it to the instance's VPC and add a default route through the gateway
+resource "aws_route_table" "ttt_rt" { 
+  vpc_id = aws_vpc.ttt_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.ttt_gateway.id
+  }
+
+  tags = {
+    Name = "ttt_rt"
+  }
+}
+
+# Associate the routing table to the subnet to apply routing rules to that subnet
+resource "aws_route_table_association" "ttt_rt_assoc" {
+  subnet_id      = aws_subnet.ttt_subnet.id
+  route_table_id = aws_route_table.ttt_rt.id
+}
+
+# Create a security group defining incoming and outcoming rules for the network
+# That way we define and enable only the necessary ports for our application
+# This security group is then attached to the VPC
+resource "aws_security_group" "ttt_sec_group" {
+  name        = "ttt_sec_group"
+  vpc_id      = aws_vpc.ttt_vpc.id
+  description = "Security group for accessing application and ec2 via SSH"
+
+  # HTTP rule
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # HTTPS rule
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp" 
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # backend rule - port 8080
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp" 
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # frontend rule - port 3000
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp" 
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # ssh - enable ssh connection
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # enable whole outcoming traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ttt_sec_group"
+  }
+}
