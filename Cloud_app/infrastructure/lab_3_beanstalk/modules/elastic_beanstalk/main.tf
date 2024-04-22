0b9af563c6999dd8e09fb8a496f386cc9d6208b4
Krzysztof Glowacz <krzysztofglowacz88@gmail.com>
2024-04-22 14:38:41 +0200

    Basic cleanup, removed old terraform infrast., deleted commented lines

--- Cloud_app/infrastructure/lab_3_beanstalk/modules/elastic_beanstalk/main.tf
+++ Cloud_app/infrastructure/lab_3_beanstalk/modules/elastic_beanstalk/main.tf
@@ -1,66 +1,14 @@
 resource "aws_s3_bucket" "l3-ttt-s3-bucket" {
   bucket = "l3-ttt-s3-bucket"
-  tags = {
+  tags   = {
     Name = "l3-ttt-s3-bucket"
   }
 }
 
-# data "local_file" "docker-compose-file" {
-#   filename = "${path.module}/docker-compose.yml"
-# }
-
-# data "archive_file" "compose-zip" {
-#   type        = "zip"
-#   output_path = "${path.module}/compose.zip"
-
-#   source {
-#     content  = <<-EOT
-#     version: '3'
-#     services:
-#         backend:
-#             image: krisenberg/backend:latest
-#             container_name: backend
-#             ports:
-#                 - "8080:80"
-#             environment:
-#                 - FRONTEND_IP="${var.method}://${var.cname_prefix}.us-east-1.elasticbeanstalk.com:3000"
-#         frontend:
-#             image: krisenberg/frontend:latest
-#             container_name: frontend
-#             ports:
-#                 - "3000:3000"
-#             environment:
-#                 - REACT_APP_BACKEND_IP="${var.method}://${var.cname_prefix}.us-east-1.elasticbeanstalk.com:8080"
-#     EOT
-#     filename = "docker-compose.yml"
-#   }
-# }
-
 resource "aws_s3_object" "l3-ttt-s3-object" {
   bucket = aws_s3_bucket.l3-ttt-s3-bucket.bucket
-  # source = data.archive_file.compose-zip.output_path
-  # key    = "compose.zip"
   source = "${path.module}/docker-compose.yml"
-  key = "docker-compose.yml"
-#   key    = "docker-compose.yml"
-#   content = <<-EOT
-#     version: '3'
-#     services:
-#     backend:
-#         image: krisenberg/backend:latest
-#         container_name: backend
-#         ports:
-#         - "8080:80"
-#         environment:
-#         - FRONTEND_IP="${var.method}://${var.cname_prefix}.us-east-1.elasticbeanstalk.com:3000"
-#     frontend:
-#         image: krisenberg/frontend:latest
-#         container_name: frontend
-#         ports:
-#         - "3000:3000"
-#         environment:
-#         - REACT_APP_BACKEND_IP="${var.method}://${var.cname_prefix}.us-east-1.elasticbeanstalk.com:8080"
-#     EOT
+  key    = "docker-compose.yml"
 }
 
 resource "aws_elastic_beanstalk_application" "l3-ttt-app" {
@@ -70,7 +18,7 @@ resource "aws_elastic_beanstalk_application" "l3-ttt-app" {
 
 resource "aws_iam_instance_profile" "instance-profile" {
   name = "instance-profile" # Instance profile name
-  role = "LabRole"                                 # Existing IAM role to associate with the instance profile
+  role = "LabRole" # Existing IAM role to associate with the instance profile
 }
 
 resource "aws_elastic_beanstalk_application_version" "l3-ttt-app-version" {
@@ -78,7 +26,6 @@ resource "aws_elastic_beanstalk_application_version" "l3-ttt-app-version" {
   application = aws_elastic_beanstalk_application.l3-ttt-app.name
   description = "Tic tac toe game running in the cloud infrastructure (using AWS Elastic Beanstalk)"
   bucket      = aws_s3_bucket.l3-ttt-s3-bucket.bucket
-  # key         = "compose.zip"
   key         = aws_s3_object.l3-ttt-s3-object.key
 }
 
@@ -96,29 +43,11 @@ resource "aws_elastic_beanstalk_environment" "l3-ttt-env" {
     value     = "SingleInstance" # Environment type, could be LoadBalanced or SingleInstance
   }
 
-  # setting {
-  #   namespace = "aws:autoscaling:asg"
-  #   name      = "MinSize"
-  #   value     = "1" # Minimum size of the Auto Scaling Group
-  # }
-
-  # setting {
-  #   namespace = "aws:autoscaling:asg"
-  #   name      = "MaxSize"
-  #   value     = "2" # Maximum size of the Auto Scaling Group
-  # }
-
   setting {
     namespace = "aws:autoscaling:launchconfiguration"
     name      = "IamInstanceProfile"
     value     = aws_iam_instance_profile.instance-profile.name
   }
-
-  # setting {
-  #   namespace = "aws:elasticbeanstalk:environment"
-  #   name      = "ServiceRole"
-  #   value     = "LabRole"
-  # }
 
   setting {
     namespace = "aws:autoscaling:launchconfiguration"
